42559efa55034d4a0e6af3702f2ffb44c0af2882
Allen G <alm.gong@gmail.com>
2015-06-10 13:22:34 -0700

    added initial pages

---
+++ index.js
@@ -0,0 +1,39 @@
+var appPath = "/public/www-data/";
+var viewPath = "/public/www-data/view"
+var express = require('express');
+var app = express();
+var path  = require('path');
+var fs = require('fs');
+
+//set up underscore + html rendering
+var cons = require('consolidate');
+app.engine('html', cons.underscore);
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, viewPath));
+
+//var dbURL = "postgres://rpfdcadokaxnvb:B7AFleOzmHR3dRLa4qWV0n4XjA@ec2-54-163-227-94.compute-1.amazonaws.com:5432/d8t4juohs69msb?ssl=true";
+app.set('port', (process.env.PORT || 5000));
+
+//parsing
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({
+//  extended: true
+//}));
+
+//static
+app.use('/static', express.static(__dirname + appPath + 'static'));
+
+/** default landing **/
+app.get('/', function(req, res) {
+	//res.send('hello')
+  res.render('index');
+});
+
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
+
+
+
+
